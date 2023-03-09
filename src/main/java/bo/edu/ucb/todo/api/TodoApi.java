bc949dee6a6b57a6fdc0232deb49616fd20e9b01
Ernesto Campohermoso <ecampohermoso@cirrus-it.net>
2023-03-09 23:36:32 +0000

    Agregando GET y PUT a todo

--- src/main/java/bo/edu/ucb/todo/api/TodoApi.java
+++ src/main/java/bo/edu/ucb/todo/api/TodoApi.java
@@ -1,9 +1,11 @@
 package bo.edu.ucb.todo.api;
 import java.util.*;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import bo.edu.ucb.todo.dto.*;
 @RestController
@@ -17,6 +19,57 @@ class TodoApi {
         response.setCode("0000");
         response.setResponse(tasks);
         return response;
+    }
+
+    @GetMapping("/api/v1/task/{idTask}")
+    public ResponseDto<TaskDto> getTaskById( @PathVariable("idTask") Integer id) {
+        ResponseDto<TaskDto> response = new ResponseDto<>();
+        //Buscamos el elemento en la lista
+        TaskDto task = tasks.stream()
+                .filter(t -> t.getTaskId().equals(id))
+                .findFirst()
+                .orElse(null);
+        // Si no existe retornamos un error
+        if (task == null) {
+            //FIXME: Cambiar el codigo de error debe retornar 404
+            response.setCode("0001");
+            response.setResponse(null);
+            response.setErrorMessage("Task not found");
+            return response;
+        } else {
+            // Si existe retornamos el elemento
+            response.setCode("0000");
+            response.setResponse(task);
+            return response;
+        }
+    }
+
+    @PutMapping("/api/v1/task/{idtask}")
+    public ResponseDto<TaskDto> updateTaskById( @PathVariable Integer idTask, @RequestBody TaskDto newTask) {
+        ResponseDto<TaskDto> response = new ResponseDto<>();
+        //Buscamos el elemento en la lista
+        TaskDto task = tasks.stream()
+                .filter(t -> t.getTaskId().equals(idTask))
+                .findFirst()
+                .orElse(null);
+        // Si no existe retornamos un error
+        if (task == null) {
+            //FIXME: Cambiar el codigo de error debe retornar 404
+            response.setCode("0001");
+            response.setResponse(null);
+            response.setErrorMessage("Task not found");
+            return response;
+        } else {
+            // Actualizamos los atributos de task con los de newTask
+            
+            task.setDescription(newTask.getDescription());
+            task.setDate(newTask.getDate());
+            task.setLabelIds(newTask.getLabelIds());
+            // Si existe retornamos el elemento
+            response.setCode("0000");
+            response.setResponse(task);
+            return response;
+        }
     }
 
     @PostMapping("/api/v1/task")
